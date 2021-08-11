3f58eff7bdcead1e5b994fad6e1edf113de323f3
wanghaihua-hub <58599570+wanghaihua-hub@users.noreply.github.com>
2021-08-11 00:26:38 -0400

    Update seqlogo_poly_tree.R

--- seqlogo_poly_tree.R
+++ seqlogo_poly_tree.R
@@ -146,3 +146,95 @@ dataly[ , -which(colnames(dataly) %in% c("b","d"))]
 dataly[ , !colnames(dataly) %in% c("b","d")]
 #删除n行的处理方式
 dataly[!rownames(dataly) %in% c("n") , ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(ape)
+library(phangorn)
+library(seqinr)
+library(ggtree)
+library(ggplot2)
+library(patchwork)
+
+rm(list = ls())
+
+setwd('F:/linux_share')
+
+test_dna = read.dna('DOE_EXP3_D1D2_suillus_polygenrtic_tree.fasta', format = 'fasta')
+test_phyDat = phyDat(test_dna, type = 'DNA', levels = NULL)
+
+# 模型评估
+mt = modelTest(test_phyDat)
+print(mt)
+
+# 计算距离
+dna_dist = dist.ml(test_phyDat,model = 'JC69')
+
+# NJ树
+tree_nj = NJ(dna_dist)
+write.tree(tree_nj)
+
+p_nj=ggtree(tree_nj) +
+  geom_tiplab(size = 1, color = 'red')+
+  labs(title = 'Neighbor Joining Tree')
+ggsave("genetree.pdf",dpi = 300, 
+       width=8, height=7, unit = "in")
+p_nj
+
+
+# UPGMA树
+tree_upgma = upgma(dna_dist)
+p_UPGMA = ggtree(tree_upgma) +
+  geom_tiplab(size = 1, color = 'black')+
+  labs(title = 'UPGMA Tree')
+ggsave("genetree.pdf",dpi = 300, 
+       width=8, height=7, unit = "in")
+p_UPGMA
+​
+# 最大简约树
+parsimony(tree_upgma, data = test_phyDat)
+parsimony(tree_nj, data = test_phyDat)
+test_optim = optim.parsimony(tree_nj, data = test_phyDat)
+tree_peatchet = pratchet(test_phyDat)
+p_Maximum_Parsimony = ggtree(tree_peatchet) +
+  geom_tiplab(size = 0.6, color = 'blue')+
+  labs(title = 'Maximum Parsimony Tree')
+p_Maximum_Parsimony
+ggsave("genetree.pdf",dpi = 300, 
+       width=8, height=7, unit = "in")
+ggt
+# 最大似然法
+tree_fit = pml(tree_nj, data = test_phyDat)
+print(tree_fit)
+fitJC = optim.pml(tree_fit, model = 'JC', rearrangement = 'stochastic')
+logLik(fitJC)
+tree_bs = bootstrap.pml(fitJC, bs = 1000, optNni = T,
+                        multicore = F, 
+                        control = pml.control(trace = 0))
+tree_ml_bootstrap = plotBS(midpoint(fitJC$tree), tree_bs, p = 50, type = 'p')
+​
+p_Maximum_Likelihood = ggtree(tree_ml_bootstrap) +
+  geom_tiplab(size = 3, color = 'purple')+
+  labs(title = 'Maximum Likelihood Tree')
+p_Maximum_Likelihood
+​
+p_all = p_nj + p_UPGMA + p_Maximum_Parsimony + p_Maximum_Likelihood +
+  plot_layout(ncol = 2)
+ggsave(p_all, filename = 'figures/all.pdf', width = 8, height = 8)
+
+
+
+
