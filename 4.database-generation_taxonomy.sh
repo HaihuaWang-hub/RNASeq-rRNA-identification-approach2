d9a45875a228cf3b50de8d7d4007cfa1c33ce324
wanghaihua-hub <58599570+wanghaihua-hub@users.noreply.github.com>
2021-08-10 22:45:00 -0400

    Update 4.database-generation_taxonomy.sh

--- 4.database-generation_taxonomy.sh
+++ 4.database-generation_taxonomy.sh
@@ -39,7 +39,7 @@ done
 
 
 #4. extract the taxid
-awk '{print $2}' accession2taxid |grep -v -w "0" taxids_0 > taxids
+awk '{print $2}' accession2taxid |grep -v -w "0" |sort -u > taxids
 wc -l taxids
 
 #5. link taxid to lineage
@@ -59,65 +59,46 @@ taxonkit reformat -P  lineage.txt \
 --data-dir taxdump \
 --delimiter ";" \
 --format "{k};{p};{c};{o};{f};{g};{s}" \
-| cut -f 1,3 > taxid2lineage
+| cut -f 1,3  > taxid2lineage
 
 
 
 #7. refine the lineage (remove the lineage without "Order" level )
- sed "/o__;f__;/d" taxid2lineage > taxid2lineage_final
+ sed "/o__;f__;/d" taxid2lineage > taxid2lineage_assigned
 
-#8. extract Order_assigned taxid
-awk '{print $1}' taxid2lineage_final > taxid_assigned
+#8. extract assigned taxid
+awk '{print $1}' taxid2lineage_assigned > taxid_assigned
 
-
-
-
-
-
-
-
-
-
+#9. extract assigned accession2taxid
 touch accession2taxid_assigned
+
 for i in $(cat taxid_assigned)
 do
-grep -w "$i" accession2taxid >> accession2taxid_assigned
+grep -r "$i" accession2taxid >> accession2taxid_assigned
 done
 
+awk '{print $1}' accession2taxid_assigned > accession_assigned
 
-awk '{print $1}' accession2taxid_assigned > accession_assigned_0
-sort -n accession_assigned_0 | uniq > accession_assigned
 
-wc -l accession
+wc -l accession.number
 wc -l accession_assigned
-
 wc -l taxid_assigned
 
-taxonkit lineage --data-dir ../taxdump taxid_assigned > lineage_assigned.txt
 
-taxonkit reformat -P --data-dir ../taxdump lineage_assigned.txt \
---delimiter ";" \
---fill-miss-rank \
---format "{k};{p};{c};{o};{f};{g};{s}" \
---miss-rank-repl "0" \
---miss-rank-repl-prefix "unclassified " \
-| cut -f 1,3 > taxid2lineage_assigned
-
-
-
-taxid2lineage_assigned
 ########################################################################
 #extract fasta with new accession ids
 
-/home/wang/genome_tools/faSomeRecords ../SILVA_138.1_LSUParc_tax_silva_trunc_assession.fasta accession_assigned SILVA_138.1_LSUParc_tax_silva_trunc_assigned.fasta
-
-seqkit rmdup SILVA_138.1_LSUParc_tax_silva_trunc_assigned.fasta  -n -o SILVA_138.1_LSUParc_tax_silva_trunc_assigned_dump_removed.fasta
-
-seqkit seq SILVA_138.1_LSUParc_tax_silva_trunc_assigned_dump_removed.fasta -w 0 > SILVA_138.1_LSUParc_tax_silva_trunc_assigned_dump_removed_singleline.fasta
+/home/wang/genome_tools/faSomeRecords SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filter_final_uniq.fasta \
+                                       accession_assigned \
+                                       SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filter_final_uniq_assigned.fasta
 
 
+seqkit seq SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filter_final_uniq_assigned.fasta -w 0 >  SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filtered_assigned.fasta 
 
 
+mv SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filtered_assigned.fasta 
+mv accession2taxid_assigned
+mv taxid2lineage_assigned
 
 
 
