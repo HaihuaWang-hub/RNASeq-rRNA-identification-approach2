11a81da6e371033f0f78796f29b36d6e942194ab
wanghaihua-hub <58599570+wanghaihua-hub@users.noreply.github.com>
2021-08-13 22:07:50 -0400

    Update seqlogo_poly_tree.R

--- seqlogo_poly_tree.R
+++ seqlogo_poly_tree.R
@@ -1,5 +1,4 @@
 
-
 #################################################################################
 #gene logo :https://cloud.tencent.com/developer/article/1511065
 #################################################################################
@@ -7,43 +6,18 @@
 #install.packages("ggseqlogo")
 #install from github
 #devtools::install.github("omarwagih/ggseqlogo")
+#install.packages("spiralize")
+
 
 library(ggplot2)
 library(ggseqlogo)
 library(seqinr)
 library(Biostrings)
-install.packages("spiralize")
-
 
 
 #read the sequence
-DNA_fasta <- read.fasta(file = "suillus_rhizopogon_database.fas", seqtype = "DNA", as.string = T)
+my_fasta <- read.fasta(file = "Boletales.aln", seqtype = "DNA", as.string = T)
 
-
-
-
-
-
-
-
-
-
-#输出fasta文件:https://blog.csdn.net/weixin_42960896/article/details/100339991
-x1 = names(my_fasta)
-res <- paste0(">",x1,"\n",my_fasta_string_1_100)
-res
-writeLines(res)
-
-write.table(res,
-            file = "test.fasta",
-            row.names = F,
-            quote = F)
-
-
-#加载数据
-data(ggseqlogo_sample)
-seqs_dna
-head(seqs_dna)[1]
 
 #将序列转为字符串向量
 my_fasta_string = vector(mode = 'character')
@@ -51,95 +25,57 @@ for (i in 1:length(my_fasta)){
   my_fasta_string[i] = toupper(c2s(my_fasta[[i]]))
 }
 
-# 使用start和end 参数获取子序列
-my_fasta_string_1_100 <-  subseq(my_fasta_string, start = 1, end = 100)
 
-#可视化
-ggseqlogo(my_fasta_string_1_100)
-ggplot()+geom_logo(seqs_dna$MA0001.1)+theme_logo()
-ggseqlogo(seqs_dna$MA0001.1)
-ggseqlogo(pfms_dna$MA0018.2)
+seq_len=nchar(my_fasta_string)[1.]
 
-#方法
-#ggseqlogo通过method选项支持两种序列标志生成方法：bits和probability。
-p1 <- ggseqlogo(seqs_dna$MA0001.1, method="bits")
-p2 <- ggseqlogo(seqs_dna$MA0001.1, method="prob")
-gridExtra::grid.arrange(p1,p2)
+#seqlogo
+ggseqlogo(my_fasta_string_1_100,seq_type = "dna", method="bits") + theme(axis.text.x = element_blank()) +
+          annotate('rect', xmin = 0.5, xmax = 95.5, ymin = -0.05, ymax = 2.2, alpha = .1, col='black', fill='red')
 
-
-
-my_fasta_string_1_100 <-  subseq(my_fasta_string, start = 1, end = 100)
-p1 <- ggseqlogo(my_fasta_string_1_100,seq_type = "dna")
+# get sequence based on the start and end site
+my_fasta_string_1_100 <-    subseq(my_fasta_string, start = 1,   end = 100)
+p1 <- ggseqlogo(my_fasta_string_1_100,seq_type = "dna", method="bits") +
+                 theme(axis.text.x = element_blank()) +
+                 labs(title = "Base site 1 - 100")
 my_fasta_string_101_200 <-  subseq(my_fasta_string, start = 101, end = 200)
-p2 <- ggseqlogo(my_fasta_string_101_200,seq_type = "dna")
+p2 <- ggseqlogo(my_fasta_string_101_200,seq_type = "dna") + 
+                theme(axis.text.x = element_blank()) +
+                labs(title = "Base site 101 - 200")
 my_fasta_string_201_300 <-  subseq(my_fasta_string, start = 201, end = 300)
-p3 <- ggseqlogo(my_fasta_string_201_300,seq_type = "dna")
+p3 <- ggseqlogo(my_fasta_string_201_300,seq_type = "dna") + 
+                theme(axis.text.x = element_blank())+
+                labs(title = "Base site 201 - 300")
 my_fasta_string_301_400 <-  subseq(my_fasta_string, start = 301, end = 400)
-p4 <- ggseqlogo(my_fasta_string_301_400,seq_type = "dna")
+p4 <- ggseqlogo(my_fasta_string_301_400,seq_type = "dna") + 
+                theme(axis.text.x = element_blank())+
+                labs(title = "Base site 301 - 400")
 my_fasta_string_401_500 <-  subseq(my_fasta_string, start = 401, end = 500)
-p5 <- ggseqlogo(my_fasta_string_401_500,seq_type = "dna")
+p5 <- ggseqlogo(my_fasta_string_401_500,seq_type = "dna") + 
+                theme(axis.text.x = element_blank())+
+                labs(title = "Base site 401 - 500")
 my_fasta_string_501_600 <-  subseq(my_fasta_string, start = 501, end = 600)
-p6 <- ggseqlogo(my_fasta_string_501_600,seq_type = "dna")
-my_fasta_string_601_700 <-  subseq(my_fasta_string, start = 601, end = 700)
-p7 <- ggseqlogo(my_fasta_string_601_700,seq_type = "dna")
+p6 <- ggseqlogo(my_fasta_string_501_600,seq_type = "dna") + 
+                theme(axis.text.x = element_blank())+
+                labs(title = "Base site 501 - 600")
+my_fasta_string_601_700 <-  subseq(my_fasta_string, start = 601, end = seq_len)
+p7 <- ggseqlogo(my_fasta_string_601_700,seq_type = "dna") + 
+                theme(axis.text.x = element_blank())+
+                labs(title = paste("Base site 601 -",seq_len,sep=" "))
+my_fasta_string_701_800 <-  subseq(my_fasta_string, start = 701, end = seq_len)
+p8 <- ggseqlogo(my_fasta_string_701_800,seq_type = "dna") + 
+                theme(axis.text.x = element_blank())+
+                 labs(title = paste("Base site 701 -",seq_len,sep=" "))
+
 
 
 gridExtra::grid.arrange(p1,p2,p3,p4,p5,p6,p7,nrow=7)
 
-#序列类型
-#ggseqlogo支持氨基酸、DNA和RNA序列类型，默认情况下ggseqlogo会自动识别数据提供的序列类型，也可以通过seq_type选项直接指定序列类型。
-ggseqlogo(seqs_aa$AKT1, seq_type="aa")
 
 
-#用数字来代替碱基
-seqs_numeric <- chartr("ATGC", "1234", seqs_dna$MA0001.1)
-ggseqlogo(seqs_numeric, method="prob", namespace=1:4)
-
-
-#配色
-#ggseqlogo可以使用col_scheme参数来设置配色方案，具体可参考?list_col_schemes
-ggseqlogo(seqs_dna$MA0001.1, col_scheme="base_pairing")
-#自定义配色
-csl <- make_col_scheme(chars = c("A","T", "C", "G"), groups = c("gr1","gr1", "gr2","gr2"), cols = c("purple","purple","blue","blue"))
-ggseqlogo(seqs_dna$MA0001.1,col_scheme=csl)
-#连续配色
-cs2 <- make_col_scheme(chars = c("A", "T", "C", "G"), values = 1:4)
-ggseqlogo(seqs_dna$MA0001.1, col_scheme=cs2)
-
-
-#同时绘制多个序列标志
-ggseqlogo(seqs_dna, ncol = 4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+rm(list=ls())
 
 ##########################################################
-#多基因联合建树---序列串联/连接
+#Multi-gene polygenetic tree
 ##########################################################
 library(ape)
 library(ggplot2)
@@ -147,8 +83,6 @@ library(ggseqlogo)
 library(seqinr)
 library(Biostrings)
 library(tidyr)
-rm(list=ls())
-setwd("D:/Desktop/bioinfomatic/2_polygenic_tree")
 
 
 data1 <- read.fasta(file = "suillus_database_seq_from_sunny_short_1.fasta", seqtype = "DNA", as.string = T)
@@ -160,20 +94,13 @@ data3 <- read.fasta(file = "suillus_database_seq_from_sunny_short_3.fasta", seqt
 
 output_0 <- cbind(data1, data2, data3, fill.with.gaps=TRUE)
 output<-output_0[ , !colnames(output_0) %in% c("fill.with.gaps")]
-write.dna(output, file="suillus_database_seq_from_sunny_short_combined.fasta", 
+write.dna(output, file="suillus_database_seq_from_sunny_short_joint.fasta", 
           format="fasta",
           append = F,
           nbcol = 10,
           colsep = "",
           colw = 1000,
-          )
-
-
-#删除b,d列的处理方式,-which 可以用！代替
-dataly[ , -which(colnames(dataly) %in% c("b","d"))]
-dataly[ , !colnames(dataly) %in% c("b","d")]
-#删除n行的处理方式
-dataly[!rownames(dataly) %in% c("n") , ]
+)
 
 
 
@@ -182,11 +109,15 @@ dataly[!rownames(dataly) %in% c("n") , ]
 
 
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("ggtree")
 
 
-
-
-
+if (!requireNamespace(c('ape','phangorn','seqinr'))) {
+  install.packages(c('ape','phangorn','seqinr'))
+}
 
 
 
@@ -197,21 +128,18 @@ library(ggtree)
 library(ggplot2)
 library(patchwork)
 
-rm(list = ls())
 
-setwd('F:/linux_share')
-
-test_dna = read.dna('DOE_EXP3_D1D2_suillus_polygenrtic_tree.fasta', format = 'fasta')
+test_dna = read.dna('Boletales.aln', format = 'fasta')
 test_phyDat = phyDat(test_dna, type = 'DNA', levels = NULL)
 
-# 模型评估
+# model evaluation
 mt = modelTest(test_phyDat)
 print(mt)
 
-# 计算距离
+# calculate the distance
 dna_dist = dist.ml(test_phyDat,model = 'JC69')
 
-# NJ树
+# NJ tree
 tree_nj = NJ(dna_dist)
 write.tree(tree_nj)
 
@@ -223,7 +151,7 @@ ggsave("genetree.pdf",dpi = 300,
 p_nj
 
 
-# UPGMA树
+# UPGMA tree
 tree_upgma = upgma(dna_dist)
 p_UPGMA = ggtree(tree_upgma) +
   geom_tiplab(size = 1, color = 'black')+
@@ -232,19 +160,20 @@ ggsave("genetree.pdf",dpi = 300,
        width=8, height=7, unit = "in")
 p_UPGMA
 ​
-# 最大简约树
+# Maximal parsimony tree
 parsimony(tree_upgma, data = test_phyDat)
 parsimony(tree_nj, data = test_phyDat)
 test_optim = optim.parsimony(tree_nj, data = test_phyDat)
 tree_peatchet = pratchet(test_phyDat)
+
 p_Maximum_Parsimony = ggtree(tree_peatchet) +
   geom_tiplab(size = 0.6, color = 'blue')+
   labs(title = 'Maximum Parsimony Tree')
 p_Maximum_Parsimony
 ggsave("genetree.pdf",dpi = 300, 
        width=8, height=7, unit = "in")
-ggt
-# 最大似然法
+
+# Maximum Likelihood Method
 tree_fit = pml(tree_nj, data = test_phyDat)
 print(tree_fit)
 fitJC = optim.pml(tree_fit, model = 'JC', rearrangement = 'stochastic')
@@ -253,15 +182,39 @@ tree_bs = bootstrap.pml(fitJC, bs = 1000, optNni = T,
                         multicore = F, 
                         control = pml.control(trace = 0))
 tree_ml_bootstrap = plotBS(midpoint(fitJC$tree), tree_bs, p = 50, type = 'p')
-​
+
 p_Maximum_Likelihood = ggtree(tree_ml_bootstrap) +
   geom_tiplab(size = 3, color = 'purple')+
   labs(title = 'Maximum Likelihood Tree')
 p_Maximum_Likelihood
-​
+
+
+#joint all method
 p_all = p_nj + p_UPGMA + p_Maximum_Parsimony + p_Maximum_Likelihood +
   plot_layout(ncol = 2)
 ggsave(p_all, filename = 'figures/all.pdf', width = 8, height = 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
