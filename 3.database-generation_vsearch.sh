401c135cb0359bd873231912a4dcedfc4645ede5
wanghaihua-hub <58599570+wanghaihua-hub@users.noreply.github.com>
2021-08-15 16:21:39 -0400

    Update 3.database-generation_vsearch.sh

--- 3.database-generation_vsearch.sh
+++ 3.database-generation_vsearch.sh
@@ -5,7 +5,7 @@ module load vsearch
 
 vsearch --cluster_size SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filter_final.fasta \
         -output SILVA_138.1_LSUParc_tax_silva_LSU_DNA_filter_final_uniq.fasta \
-        --id 1 \
+        --id 0.99999 \
         --centroids \
         --xsize \
         --threads 20 \
