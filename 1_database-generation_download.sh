a457d44c7a60be0264df196b854e2c84469e55d6
Haihua <58599570+HaihuaWang-hub@users.noreply.github.com>
2021-09-05 23:53:31 -0400

    Update 1_database-generation_download.sh

--- 1_database-generation_download.sh
+++ 1_database-generation_download.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -l
+#SBATCH --job-name=LSU_database_generation_download      # Job name
+#SBATCH --mail-type=all            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=wanghaihua@ufl.edu       # Where to send mail	
+#SBATCH --ntasks=1                      # Number of MPI ranks
+#SBATCH --cpus-per-task=9               # Number of cores per MPI rank 
+#SBATCH --nodes=1                       # Number of nodes
+#SBATCH --ntasks-per-node=1             # How many tasks on each node
+#SBATCH --ntasks-per-socket=1           # How many tasks on each CPU or socket
+#SBATCH --mem-per-cpu=7G             # Memory per core
+#SBATCH --time=30-00:00:00                 # Time limit hrs:min:sec
+#SBATCH --output=LSU_database_generation_download_%j.log     # Standard output and error log
 
+pwd; hostname; date
+module load seqkit
 
-CPU="8"
+CPU="18"
 
 #This is the code for generation of mulit- short database D1D2 region
-
 
 ###############################################################
 #Download the raw LSU database #
@@ -13,7 +25,7 @@ CPU="8"
 #The database was downloaded from SILVA collected un-filtered database 
 #As well, the sequences could be collected from International Nucleotide Sequence Database Collaboration (INSDC) databases
 
-workdir="******************"
+workdir="/home/wanghaihua/all_data/database_generation"
 mkdir $work_dir/raw_database
 cd $work_dir/raw_database
 
@@ -21,5 +33,5 @@ wget -c https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA
 gunzip $work_dir/raw_database/SILVA_138.1_LSUParc_tax_silva.fasta.gz
 
 #RNA to DNA
-seqkit seq --threads 4 --rna2dna SILVA_138.1_LSUParc_tax_silva.fasta > SILVA_138.1_LSUParc_tax_silva_DNA.fasta
+seqkit seq --threads 10 --rna2dna SILVA_138.1_LSUParc_tax_silva.fasta > SILVA_138.1_LSUParc_tax_silva_DNA.fasta
 
